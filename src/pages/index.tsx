7b708285776230ff422fb6a2b403286acf2df329
Olivier Romanetti <olivier.romanetti@alithya.com>
2023-10-24 12:42:39 -0400

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,11 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
 
+  const { data } = api.post.getAll.useQuery();
+
+  console.log(data);
   return (
     <>
       <Head>
@@ -20,6 +21,9 @@ export default function Home() {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
         </div>
       </main>
     </>
